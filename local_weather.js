10c727f710bf64cf84a34475a9e585ca3bdbd905
jkappel71 <jkappel71@users.noreply.github.com>
2018-10-03 13:16:25 -0600

    Update local_weather.js

--- local_weather.js
+++ local_weather.js
@@ -23,5 +23,6 @@ makeRequest('GET', 'https://www.metaweather.com/api/location/2383489/', function
 	let curtemp = round(data.consolidated_weather[0].the_temp * (9/5) + 32, 1);
 	let maxtemp = round(data.consolidated_weather[0].max_temp * (9/5) + 32, 1);
 
-	forcast += 'Currently its '+curtemp+ ' degrees in Colorado Springs. Todays high is expected to be '+maxtemp+' degrees.';  
+	forcast += 'Currently its '+curtemp+ ' degrees in Colorado Springs. Todays high is expected to be '+maxtemp+' degrees.';
+	alert(forcast);
 });
